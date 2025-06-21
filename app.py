b1d97989db201a14d1460666dbc554490836c8ff
Henry <henry@testing.com>
2025-06-22 00:39:21 +0200

    Make Flask app Render-compatible

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def serve_whitelist():
         return Response(f.read(), mimetype='text/plain')
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
